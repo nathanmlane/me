b44edb05d40b6fcc299639c33ffb9128bf9c0372
Nate Lane <nate@sidekicklouisville.com>
2019-03-18 20:58:03 -0400

    fixed mobile menu issue and toggle button placement

--- js/chungus.js
+++ js/chungus.js
@@ -1,6 +1,9 @@
 //Custom JS -- Mobile Menu
 
 function openMobileNav() {
+
+  //Selects mobile-nav div, which defaults to display:none; | toggling it between display: flex and none
+
   var nav = document.getElementById("mobile-nav");
   if (nav.style.display === "flex") {
     nav.style.display = "none";
@@ -12,7 +15,15 @@ function openMobileNav() {
   header.classList.toggle("toggle-menu");
 
   var close = document.getElementById("close");
-  close.classList.toggle("close")
+  close.classList.toggle("close");
+
+  var x = document.getElementById("hide-bg");
+  if (x.style.visibility === "hidden") {
+    x.style.visibility = "visible";
+  } else {
+    x.style.visibility = "hidden";
+  }
+
 };
 
 // JQuery Function -- Anchor Link Scroll -- https://codepen.io/nxworld/pen/OyRrGy
@@ -26,7 +37,7 @@ $(function() {
 
 // JQuery Function -- Dark Mode
 
-$("#sun-moon").click(function(){
+let toggleDarkMode = function() {
   $("body").toggleClass('night day');
   $("p").toggleClass('dark light');
   $("h1").toggleClass('dark light');
@@ -38,4 +49,7 @@ $("#sun-moon").click(function(){
   $("li").toggleClass('dark light');
   $("a").toggleClass('dark light');
   $("button").toggleClass('dark light');
-});
+  $("nav").toggleClass('dark light');
+}
+
+$("#sun-moon").click(toggleDarkMode);
